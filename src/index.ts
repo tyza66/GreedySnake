b8cf4824fca61a9ab351b5263b6e43d1c4aed229
洮羱芝闇 <shun_@outlook.com>
2022-05-08 12:17:24 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import './style/index.less';
 import GameControl  from './moduls/GameControl';
 
-
-/*
 console.log("hello tyza66");
 const gc = new GameControl();
+
+/*
 setInterval(() =>{
     console.log(gc.direction);
 },1000);
 */
-
 
 /* 测试Food类
 const food = new Food();
